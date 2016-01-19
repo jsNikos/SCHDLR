94813b6a702c0c261e55396265a4045b34d3b01b
shumwayjs <alf.shumway7@gmail.com>
2016-01-19 07:24:07 -0500

    drag drop something works

--- public/webscheduler/js/shiftEditor/timeline/TimelineComponent.js
+++ public/webscheduler/js/shiftEditor/timeline/TimelineComponent.js
@@ -29,21 +29,83 @@ define(['text!shiftEditor/timeline/timeline.html',
           showUnavailSlot: function(timeSlot) {
             return timeSlot.shift && timeSlot.unavails.length > 0;
           },
-          handleDrag: function(timeSlot, idx) {
+          handleDrag: function(timeSlot, idx, prevIdx) {
+            var prev = prevIdx && vueScope.$data.model.timeSlots[prevIdx]; // previous drag target
+            var left = vueScope.$data.model.timeSlots[idx - 1];
+            var right = vueScope.$data.model.timeSlots[idx + 1];
+            var hasLeft = left && left.shift;
+            var hasRight = right && right.shift;
+
+            // only one slot
+            if (timeSlot.shiftStarts && timeSlot.shiftEnds) {
+              unassign(timeSlot);
+              return;
+            }
+
+            // no further handle of start or end
+            if(timeSlot.shiftStarts || timeSlot.shiftEnds){
+              return;
+            }
+
+            // starting new shift
+            if (!timeSlot.shift && !hasLeft && !hasRight) {
+              assign(timeSlot, true, true);
+              return;
+            }
+
+            // shift has at least 2 slots
             if (timeSlot.shift) {
-              timeSlot.shift = undefined;
-              timeSlot.shiftStarts = undefined;
-              timeSlot.shiftEnds = undefined;
-            } else {
-              timeSlot.shift = true;
-              timeSlot.shiftStarts = idx === 0 || !vueScope.$data.model.timeSlots[idx - 1].shift;
-              timeSlot.shiftEnds = idx === (vueScope.$data.model.timeSlots.length - 1) || !vueScope.$data.model.timeSlots[idx + 1].shift;
+              // drag end to left
+              if (hasRight && right.shiftEnds) {
+                unassign(right);
+                assign(timeSlot, undefined, true);
+                return;
+              }
+
+              // drag start to right
+              if (hasLeft && left.shiftStarts) {
+                unassign(left);
+                assign(timeSlot, true, undefined);
+                return;
+              }
+            }
+
+            if (!timeSlot.shift) {
+              // drag start to left
+              if (hasRight) {
+                assign(timeSlot, true, false);
+                assign(right, false, undefined);
+                return;
+              }
+
+              // drag end to right
+              if (hasLeft) {
+                assign(timeSlot, false, true);
+                assign(left, undefined, false);
+                return;
+              }
             }
           },
           handleDragEnd: function() {
             console.log('dragend');
             //TODO
           }
+        }
+      }
+
+      function unassign(timeSlot) {
+        timeSlot.shift = undefined;
+        timeSlot.shiftStarts = undefined;
+        timeSlot.shiftEnds = undefined;
+      }
+
+      function assign(timeSlot, start, end) {
+        timeSlot.shift = true;
+        if (start !== undefined) {
+          timeSlot.shiftStarts = start;
+        }
+        if (end !== undefined) {
+          timeSlot.shiftEnds = end;
         }
       }
 
@@ -57,16 +119,17 @@ define(['text!shiftEditor/timeline/timeline.html',
             invalidDragStart = checkDragStartInvalid(idx, vueScope.$data.model.timeSlots[idx]);
           })
           .on('drag', '.timeslot-cell', function(event, dragprops) {
-            console.log(dragprops.deltaX);
             if (invalidDragStart) {
               return;
             }
             var idx = jQuery(event.target).data('idx');
+            // ensure to trigger drag only once per cell
             if (currDraggedTimeSlotIdx === idx) {
               return;
             }
+            var prevIdx = currDraggedTimeSlotIdx;
             currDraggedTimeSlotIdx = idx;
-            vueScope.handleDrag(vueScope.$data.model.timeSlots[idx], idx);
+            vueScope.handleDrag(vueScope.$data.model.timeSlots[idx], idx, prevIdx);
           })
           .on('dragend', function(event) {
             currDraggedTimeSlotIdx = undefined;
