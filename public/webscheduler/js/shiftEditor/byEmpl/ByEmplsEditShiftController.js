cd6bd61d54a11304db7d1f5cfc16bd4a3d3dfe9e
shumwayjs <alf.shumway7@gmail.com>
2016-01-20 07:49:51 -0500

    editor template into own file

--- public/webscheduler/js/shiftEditor/byEmpl/ByEmplsEditShiftController.js
+++ public/webscheduler/js/shiftEditor/byEmpl/ByEmplsEditShiftController.js
@@ -1,50 +1,53 @@
-define(['EditShiftController', 'ByEmplsEditShiftView'], function(EditShiftController, ByEmplsEditShiftView){
+define(['EditShiftController',
+ 'ByEmplsEditShiftView',
+'text!shiftEditor/byEmpl/byEmplsEditDialog.html'],
+function(EditShiftController, ByEmplsEditShiftView, byEmplsEditDialogHtml){
 	return function(args){
 		ByEmplsEditShiftController.prototype = new EditShiftController();
 		return new ByEmplsEditShiftController(args);
 	};
-	
+
 	/**
 	 * Extention of EditShiftController supporting byEmployee -view.
 	 */
 	function ByEmplsEditShiftController(args){
 		var scope = this;
 		this.tableController = args.controller;
-		
+
 		function init(){
 			scope.init.call(scope);
 		}
-		
+
 		/**
 		 * Points to the template backing the dialog.
 		 * @returns  {_.template}
 		 */
 		this.getDialogTmpl = function(){
-			return _.template(jQuery('#byEmplsEditDialog').text());
+			return _.template(byEmplsEditDialogHtml);
 		};
-		
+
 		/**
 		 * Returns the constructor to be used to init view.
 		 */
 		this.getViewConstructor = function(){
 			return ByEmplsEditShiftView;
 		};
-		
+
 		/**
 		 * Adds employeeName and role to given scheduleDetail, extracted from
 		 * model and given selection.
 		 * @param scheduleDetail
-		 * @param selection		  
+		 * @param selection
 		 */
 		this.addRoleAndEmplFromSelections = function(scheduleDetail, selection) {
 			scheduleDetail.employeeName = scope.employeeName;
 			scheduleDetail.role = {
 				name : selection.role
 			};
-		};	
-		
+		};
+
 		init();
-		
+
 	}
-	
+
 });
--- public/webscheduler/js/shiftEditor/byRole/ByRolesEditShiftController.js
+++ public/webscheduler/js/shiftEditor/byRole/ByRolesEditShiftController.js
@@ -15,16 +15,16 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 	function ByRolesEditShiftController(args){
 		var scope = this;
 		this.tableController = args.controller;
-		
-		//model		
-		this.role = undefined; // the selected role	from shift	
+
+		//model
+		this.role = undefined; // the selected role	from shift
 		this.employees = undefined; // empl. avail. for this role
 		this.unavailableEmployees = undefined; // [EmployeeHolder], are displayed in unavail-tab
-		
+
 		function init(){
 			scope.init.call(scope);
 		}
-		
+
 		/**
 		 * Points to the template backing the dialog.
 		 * @returns  {_.template}
@@ -32,7 +32,7 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 		this.getDialogTmpl = function(){
 			return _.template(byRolesEditDialogHtml);
 		};
-		
+
 		/**
 		 * Handles change of who-selection.
 		 * @param employee : EmployeeHolder
@@ -41,19 +41,19 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 			if(!employee){
 				return;
 			}
-			// request unavails for selected employee					
-			fetchUnavailsForEmployee(employee.name, function(unavails){				
+			// request unavails for selected employee
+			fetchUnavailsForEmployee(employee.name, function(unavails){
 				scope.editShiftView.renderUnavailabilities(unavails);
 				(scope.modifiable !== false) && scope.editShiftView.enableApply();
 				scope.editShiftView.renderSelectedEmployee(employee);
-			});			
+			});
 			scope.editShiftView.closeWhoSelector();
-			scope.editShiftView.disableApply();	
+			scope.editShiftView.disableApply();
 		};
-		
+
 		/**
 		 * Fetches unavailabilities for given employee.
-		 * 
+		 *
 		 * @param employeeName
 		 * @param callback : [unavailibilities]
 		 */
@@ -73,7 +73,7 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 				}
 			});
 		}
-		
+
 		/**
 		 * Fetches unavailable employees for given role in given period.
 		 * @param args : {role : String,
@@ -81,7 +81,7 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 		 * 				 endTime,
 		 * 				 scheduleDetail : ScheduleDetailHolder (in case of an edit)
 		 * 				 }
-		 * @param promise : resolving with {unavailableEmployees : [EmployeeHolder]} 
+		 * @param promise : resolving with {unavailableEmployees : [EmployeeHolder]}
 		 */
 		function fetchUnavailableEmployees(args){
 			var data = _.clone(args);
@@ -95,37 +95,37 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 				data : data
 			});
 		}
-		
+
 		/**
 		 * Returns the constructor to be used to init view.
 		 */
 		this.getViewConstructor = function(){
 			return ByRolesEditShiftView;
-		};		
-		
+		};
+
 		/**
 		 * Adds employeeName and role to given scheduleDetail, extracted from
 		 * model and given selection.
 		 * @param scheduleDetail
-		 * @param selection		  
+		 * @param selection
 		 */
 		this.addRoleAndEmplFromSelections = function(scheduleDetail, selection) {
 			scheduleDetail.employeeName = selection.employeeName;
 			scheduleDetail.role = {
 				name : scope.role
 			};
-		};	
-		
+		};
+
 		/**
 		 * @override
-		 * Updates the model based on the given server-response.		  
+		 * Updates the model based on the given server-response.
 		 * @param resp
 		 */
-		this.updateModel = function(resp){			
+		this.updateModel = function(resp){
 			Object.getPrototypeOf(scope).updateModel.call(this, resp);
-			scope.unavailableEmployees = resp.unavailableEmployees;			
+			scope.unavailableEmployees = resp.unavailableEmployees;
 		};
-		
+
 		/**
 		 * Returns a list of employees which are available.
 		 * @returns
@@ -135,25 +135,25 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 								.union(scope.unavailableEmployees.posted)
 								.pluck('name')
 								.value();
-			return _.filter(scope.employees, function(employee){ return !_.contains(unavailNames, employee.name); }); 
+			return _.filter(scope.employees, function(employee){ return !_.contains(unavailNames, employee.name); });
 		};
-		
+
 		/**
 		 * Returns a list of employees which are not available and status is 'approved'.
 		 * @returns
 		 */
 		this.findApprovedUnavailableEmployees = function(){
 			return scope.unavailableEmployees.approved;
-		};		
-		
+		};
+
 		/**
 		 * Returns a list of employees which are not available and status is 'posted'.
 		 * @returns
 		 */
 		this.findPostedUnavailableEmployees = function(){
 			return scope.unavailableEmployees.posted;
-		};			
-		
+		};
+
 		/**
 		 * Returns the employee to which the shift is assign which is opened for edit.
 		 * @returns EmployeeHolder
@@ -161,25 +161,25 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 		this.findEmployeeFromEditedShift = function(){
 			return scope.scheduleDetail && scope.tableController.findEmployee(scope.scheduleDetail.employeeName);
 		};
-		
+
 		/**
 		 * @override
 		 * @param hour
 		 * @param minute
 		 */
 		this.handleFromSelected = function(hour, minute){
-			Object.getPrototypeOf(scope).handleFromSelected.call(this, hour, minute);			
+			Object.getPrototypeOf(scope).handleFromSelected.call(this, hour, minute);
 		};
-		
+
 		/**
 		 * @override
 		 * @param hour
 		 * @param minute
 		 */
 		this.handleToSelected = function(hour, minute){
-			Object.getPrototypeOf(scope).handleToSelected.call(this, hour, minute);			
+			Object.getPrototypeOf(scope).handleToSelected.call(this, hour, minute);
 		};
-		
+
 		/**
 		 * @override
 		 * Handles timepicker-closing by updating who-select.
@@ -187,25 +187,25 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 		this.handleTimePickerClose = function(){
 			allowEditWho() && updateWhoSelect();
 		};
-		
+
 		/**
 		 * Triggers to update who-select for the current period and role. First fetches
 		 * unavailable-employees from server for this parameter.
 		 * This is async.!
 		 */
-		function updateWhoSelect(){			 
-			var reqParams = {role : scope.role,				  				
+		function updateWhoSelect(){
+			var reqParams = {role : scope.role,
 					scheduleDetail : scope.scheduleDetail
 			};
 			_.extend(reqParams, scope.findPeriodToSubmit());
 			scope.editShiftView.disableWhoSelect();
 			fetchUnavailableEmployees(reqParams).then(function(resp){
-				scope.unavailableEmployees = resp.unavailableEmployees;			
+				scope.unavailableEmployees = resp.unavailableEmployees;
 				scope.editShiftView.updateSelectWhoTab();
 				scope.editShiftView.enableWhoSelect();
 			});
 		}
-		
+
 		/**
 		 * Checks if who-inpute can be enabled. For this the period
 		 * must be selected.
@@ -214,9 +214,9 @@ function(EditShiftController, ByRolesEditShiftView, byRolesEditDialogHtml, timeZ
 		function allowEditWho(){
 			return !!(scope.selectedStartTime && scope.selectedEndTime);
 		}
-		
+
 		init();
-		
+
 	}
-	
+
 });
