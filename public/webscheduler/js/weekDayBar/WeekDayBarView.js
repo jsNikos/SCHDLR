dd56c04148ebcffd7fe95bab5076470bf1f3cc9c
shumwayjs <alf.shumway7@gmail.com>
2016-03-07 02:45:05 -0500

    fixing weekdaybar

--- public/webscheduler/js/weekDayBar/WeekDayBarView.js
+++ public/webscheduler/js/weekDayBar/WeekDayBarView.js
@@ -5,59 +5,59 @@ define(['text!weekDayBar/weekDayBar.html',
         'css!weekDayBar/weekDayBar.css'],
 function(weekDayBarHtml, timeZoneUtils, WeekPicker){
 	return WeekDayBarView;
-	
+
 	/**
 	 * @param : args {controller: WeekDayBarController, $el : dom-context)
 	 */
 	function WeekDayBarView(args){
 		var scope = this;
-		var controller = args.controller;		
+		var controller = args.controller;
 		this.weekPicker = undefined; // WeekPicker
-		var $el = args.$el; // el which this component is attached		
-		
+		var $el = args.$el; // el which this component is attached
+
 		var periodSelectTmpl = _.template('<div class="period-select" data-weekday="<%- day %>"><%- label %></div>');
-				
+
 		/**
 		 * Adds weekDayBar to el.
 		 */
-		this.show = function(){			
-			$el.append(_.template(weekDayBarHtml, {
+		this.show = function(){
+			$el.append(_.template(weekDayBarHtml)({
 				week : 'Week of ' + formatSelectedWeek(),
-				renderPeriods : renderPeriods		
+				renderPeriods : renderPeriods
 			}));
 			initPeriodSelect();
 			initWeekSelect();
 		};
-		
+
 		/**
 		 * Inits the week-selection.
 		 */
-		function initWeekSelect(){			
+		function initWeekSelect(){
 			scope.weekPicker = new WeekPicker({
 				$el : $el.find('.datePicker'),
 				selectedDate : new Date(controller.week.startOfWeek),
 				startOfWeekDay: controller.startOfWeekDay
-			});	 
+			});
 			$el.find('.week .change').on('click', function(){
 				scope.weekPicker.getEl().slideToggle();
 			});
-			
+
 			scope.weekPicker.on('dateSelected', function(weekPicker){
 				weekPicker.getEl().slideToggle(function(){
-					controller.handleWeekSelect(weekPicker.selectedDate);	
-				});				
-			});			
-			
+					controller.handleWeekSelect(weekPicker.selectedDate);
+				});
+			});
+
 			$el.find('.week .previous').on('click', onArrowClicked);
-			$el.find('.week .next').on('click', onArrowClicked);	
+			$el.find('.week .next').on('click', onArrowClicked);
 			function onArrowClicked(event){
-				var $target = jQuery(event.target); 					
+				var $target = jQuery(event.target);
 				controller.handleWeekArrowClick($target.hasClass('previous') ? -1 : 1);
-			}			
+			}
 		}
-		
+
 		/**
-		 * Updates week-container and period-select container with current model. 
+		 * Updates week-container and period-select container with current model.
 		 */
 		this.updateDateSelectors = function(){
 			$el.find('.week .change').text('Week of '+formatSelectedWeek());
@@ -67,18 +67,18 @@ function(weekDayBarHtml, timeZoneUtils, WeekPicker){
 			scope.selectPeriod(controller.currentDay);
 			return this;
 		};
-		
+
 		/**
 		 * Inits period selection by registering click-listeners and pre-selecting.
 		 */
-		function initPeriodSelect(){ 
+		function initPeriodSelect(){
 			$el.on('click', '.period-select', function(event){
-				var weekDay = jQuery(event.target).attr('data-weekday');				
+				var weekDay = jQuery(event.target).attr('data-weekday');
 				controller.handleChangePeriod(weekDay);
 			});
 			scope.selectPeriod(controller.currentDay);
-		}		
-		
+		}
+
 		/**
 		 * Changes selection-status on given day, '' is week.
 		 * @param day : String
@@ -87,11 +87,11 @@ function(weekDayBarHtml, timeZoneUtils, WeekPicker){
 			$el.find('.period-select').removeClass('selected');
 			$el.find('.period-select[data-weekday="'+day+'"]').addClass('selected');
 		};
-		
+
 		/**
 		 * Returns html for selectable perdios (week, days)
 		 */
-		function renderPeriods(){			
+		function renderPeriods(){
 			var $holder = jQuery('<div></div>');
 			$holder.append(periodSelectTmpl({day: '', label: 'Week'}));
 			_.chain(controller.week.weekDays)
@@ -102,11 +102,11 @@ function(weekDayBarHtml, timeZoneUtils, WeekPicker){
 								   };
 						 })
 					.each(function(info){ /* render in template */
-						$holder.append(periodSelectTmpl(info));					
-					});					
+						$holder.append(periodSelectTmpl(info));
+					});
 			return $holder.html();
 		}
-		
+
 		/**
 		 * Formats week (selected week) for displaying in header.
 		 * @returns
